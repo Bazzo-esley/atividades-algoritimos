c88fc68e0ec209f7af8ca0c7c82b4bec57971704
Bazzo-esley <esley18@hotmail.com>
2022-09-02 16:09:00 -0300

    atividade 1 TDE04

---
+++ atv1.py
@@ -0,0 +1,8 @@
+"""*Faça um programa que leia um valor inteiro (que representa o real, moeda nacional) e calcule
+qual o menor número possível de notas/moedas de 100, 50, 20, 10, 5, 2 e 1 em que o valor lido
+pode ser decomposto. Escrever o valor lido e a relação de notas necessárias.*"""
+
+
+num1 = int(input(f"Digite um valor inteiro: ")
+
+num1 = 
